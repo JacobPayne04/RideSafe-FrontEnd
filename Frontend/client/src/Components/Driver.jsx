14f2857b049911c15ae9537f23cfad2c945ffc53
jacob Payne <jacob.pa7ne@Gmail.com>
2024-11-05 21:34:02 -0600

    Added most of the code for displaying drivers, there is a problem with our format that its asking for from the database

--- Frontend/client/src/Components/Driver.jsx
+++ Frontend/client/src/Components/Driver.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import '../Styling/Driver.css'
 import axios from 'axios'
 
 
 const Driver = () => {
+  const [drivers,setDrivers] = useState([])
+
+  useEffect(()=>{
+    axios.get('http://localhost:8080/drivers')
+    .then(response =>{
+      setDrivers(response.data);
+      console.log('Response from server:', response.data)
+    })
+    .catch(error => {
+      console.log("There was an error!",error);
+    });
+  },[]);
+
+
+
   return (
-    <div>Driver</div>
+    <div>
+      <h1>All Drivers</h1>
+      <ul className="driver-list">
+        {drivers.map(driver => (
+          <li key={driver._id} className="driver-details">
+            <span className="driver-name">
+              {driver.firstName} {driver.lastName}
+            </span>
+            <span className="driver-email">{driver.email}</span>
+            <span className="driver-license">{driver.licensePlate}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
