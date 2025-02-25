df6149bd66f353508755bbb593ea6f27928e1915
jacob Payne <jacob.pa7ne@Gmail.com>
2025-02-25 13:42:43 -0800

    FIX* locale storage issue

--- Frontend/client/src/Components/GoogleSignIn.jsx
+++ Frontend/client/src/Components/GoogleSignIn.jsx
@@ -14,7 +14,8 @@ const GoogleSignIn = () => {
         if (response.credential) {
             const decodedToken = jwtDecode(response.credential);
             const { email, sub: googleId } = decodedToken;
-            const data = { email, googleId: response.credential }; // Send the actual ID token
+            const data = { email, googleId, idToken: response.credential };// Send the actual ID token
+            
     
             localStorage.setItem("user", JSON.stringify(data));
     
@@ -22,9 +23,10 @@ const GoogleSignIn = () => {
                 const res = await axios.post(`http://localhost:8080/signup/${role}/googleId`, data);
                 if (res.data.exists) {
                     const driverId = localStorage.getItem("driverId");
+                    console.log("Driver ID:", driverId + {role});
                     navigate(`/driver/home/${driverId}`);
                 } else {
-                    navigate(`/register/${role}/google`);
+                    console.log("User does not exist in the database");
                 }
             } catch (error) {
                 console.error("Error during Google Sign-In process:", error);
