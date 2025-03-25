c37f41086d7d5c81d7b814392d9ab5ea526a6704
jacob Payne <jacob.pa7ne@Gmail.com>
2025-03-25 16:48:19 -0500

    UPDATE* small variable name change and outline update*

--- Frontend/client/src/Components/CheckoutForm.jsx
+++ Frontend/client/src/Components/CheckoutForm.jsx
@@ -58,7 +58,7 @@ const CheckoutForm = () => {
       const response = await fetch("/api/create-payment-intent", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ amount: rideAmount, rideId })
+        body: JSON.stringify({amount: rideAmount, paymentRequestRideId: rideId})
       });
   
       const { clientSecret } = await response.json();
