84fcccafa8c301b17e6ebb4be2636b5402ab9fd8
jacob Payne <jacob.pa7ne@Gmail.com>
2025-05-24 19:20:12 -0500

    Made jsx for Stripe account set up

---
+++ Frontend/client/src/Components/DriverStripeAccountSignUpLink.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import axios from 'axios'
+
+
+
+const DriverStripeAccountSignUpLink = () => {
+  return (
+    <div>DriverStripeAccountSignUpLink
+            <button>SignUp for Stripe and link account</button>
+    </div>
+    
+
+
+)
+}
+
+export default DriverStripeAccountSignUpLink
