c4e43c5cc041d75eb4e7336c3c4bb9756c857747
jacob Payne <jacob.pa7ne@Gmail.com>
2025-05-24 19:59:16 -0500

    Stripe link api call to generate link

--- Frontend/client/src/Components/DriverStripeAccountSignUpLink.jsx
+++ Frontend/client/src/Components/DriverStripeAccountSignUpLink.jsx
@@ -4,9 +4,19 @@ import axios from 'axios'
 
 
 const DriverStripeAccountSignUpLink = () => {
+
+    const onboardStripe = async () => {
+  const res = await fetch("/api/stripe/onboard?email=" + userEmail, {
+    method: "POST",
+  });
+  const url = await res.text();
+  window.location.href = url; // send them to Stripe onboarding
+};
+
   return (
     <div>DriverStripeAccountSignUpLink
-            <button>SignUp for Stripe and link account</button>
+            <button onClick={onboardStripe}>Finish Stripe Setup</button>
+
     </div>
     
 
