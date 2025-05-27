08aaf8ec2af455b4819412dcbff9abf7e6f0904e
jacob Payne <jacob.pa7ne@Gmail.com>
2025-05-27 18:38:08 -0500

    fixed signup

--- Frontend/client/src/Components/DriverStripeAccountSignUpLink.jsx
+++ Frontend/client/src/Components/DriverStripeAccountSignUpLink.jsx
@@ -6,34 +6,6 @@ import '../Styling/DriverStripeAccountSignUpLink.css'
 
 
 const DriverStripeAccountSignUpLink = () => {
-  const onboardStripe = async () => {
-    try {
-      const email = localStorage.getItem("userEmail");
-
-      if (!email) {
-        alert("Email not found in localStorage. Please log in again.");
-        return;
-      }
-
-      const res = await fetch("http://localhost:8080/Driver/stripe/signup?email=" + email, {
-        method: "POST",
-        
-      });
-       console.log("Using email:", email)
-      if (!res.ok) {
-       
-        throw new Error("Failed to get Stripe onboarding link.");
-       
-      }
-
-      const url = await res.text();
-      window.location.href = url;
-    } catch (error) {
-      
-      console.error("Stripe onboarding failed:", error);
-      alert("Stripe setup failed. Check console for details.");
-    }
-  };
 
   const driverEmail = localStorage.getItem('driverEmail')
   const driverId = localStorage.getItem('driverId');
